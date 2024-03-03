123ebfb231fea2c1b571b3917b5eefcbb15e9f49
fue19389 <fue19389@uvg.edu.gt>
2024-03-02 22:56:04 -0600

    Inicio de pruebas para la GUI

---
+++ miscTEST.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------
+# ----- Librerías a utilizar ---------------------
+# ------------------------------------------------
+
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sn
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+
+from tensorflow import keras
+from tensorflow.keras.optimizers import SGD
+
+
