bff89299468abc79cccbbecf4f93d58ad5fb5c4a
fue19389 <fue19389@uvg.edu.gt>
2024-03-02 22:52:40 -0600

    Ahora también está listo el código de las pruebas en vivo, de la forma OOP

--- liveTEST.py
+++ liveTEST.py
@@ -10,81 +10,75 @@ from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
 import matplotlib.pyplot as plt
 
-# ------------------------------------------------------
-# -----------------Selección de modelo------------------
-# ------------------------------------------------------
+class UseModel:
 
-# Actualmente, se tienen modelos del 0 -> 6
-n_model = 4
-# -----------------------------------------------------
+    def __init__(self):
+        # ------------------------------------------------------
+        # -----------------Selección de modelo------------------
+        # ------------------------------------------------------
+        
+        # Actualmente, se tienen modelos del 0 -> 6
+        self.n_model = 4
+        # -----------------------------------------------------
 
-# ------------------------------------------------
-# ----- Directorios a utilizar -------------------
-# ------------------------------------------------
+        # ------------------------------------------------
+        # ----- Directorios a utilizar -------------------
+        # ------------------------------------------------
+        
+        # Directorio para guardar las variables a exportar
+        self.expordir = r'C:\Users\gerar\PycharmProjects\EXPOR_TESIS'
 
-# Directorio para guardar las variables a exportar
-expordir = r'C:\Users\gerar\PycharmProjects\EXPOR_TESIS'
+    # -----------------------------------------------------
+    # -------------------Cargar modelo---------------------
+    # -----------------------------------------------------
 
-# -----------------------------------------------------
-# -------------------Cargar modelo---------------------
-# -----------------------------------------------------
+        if self.n_model == 0:
+            self.mname = 'head_or.keras'
+        elif self.n_model == 1:
+            self.mname = 'head_or1.keras'
+        elif self.n_model == 2:
+            self.mname = 'head_or2.keras'
+        elif self.n_model == 3:
+            self.mname = 'head_or3.keras'
+        elif self.n_model == 4:
+            self.mname = 'head_or4.keras'
+        elif self.n_model == 5:
+            self.mname = 'head_or5.keras'
+        elif self.n_model == 6:
+            self.mname = 'head_or6.keras'
 
-if n_model == 0:
-    mname = 'head_or.keras'
-elif n_model == 1:
-    mname = 'head_or1.keras'
-elif n_model == 2:
-    mname = 'head_or2.keras'
-elif n_model == 3:
-    mname = 'head_or3.keras'
-elif n_model == 4:
-    mname = 'head_or4.keras'
-elif n_model == 5:
-    mname = 'head_or5.keras'
-elif n_model == 6:
-    mname = 'head_or6.keras'
+        self.ho_model = os.path.join(self.expordir, self.mname)
+        self.ho_model = tf.keras.models.load_model(self.ho_model)
 
-ho_model = os.path.join(expordir, mname)
-ho_model = tf.keras.models.load_model(ho_model)
+         
+    def start(self):
 
-# -----------------------------------------------------
+        # -----------------------------------------------------
+        # ---------Inicialización de la Webcam---- ------------
+        # -----------------------------------------------------
 
-# -----------------------------------------------------
-# ---------Inicialización de la Webcam---- ------------
-# -----------------------------------------------------
-
-# # Capture webcam
-#
-cap = cv2.VideoCapture(0)
-
-# Capture webcam & Set resolution
-
-# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
-
-# ----------------------------------------------------
-
-# -----------------------------------------------------
-# ----Visualizar el predict del modelo LIVE------------
-# -----------------------------------------------------
-
-while True:
-
-    # Saving captured image and transforming from BGR TO RGB
-
-    _, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    imgRGB = cv2.resize(imgRGB, (320, 180))
-    imgRGB = imgRGB.astype(int)/255
-
-    y_predicted = ho_model.predict(np.array([imgRGB]), verbose=None)
-    prediction = np.argmax(y_predicted)
-    print(prediction)
-
-    # Show the complete image
-    cv2.imshow('Image', img)
-
-    key = cv2.waitKey(30)
-    if key == 27: # 13 = enter, 27 = esc
-        break
+        cap = cv2.VideoCapture(0)
+        
+        # -----------------------------------------------------
+        # ----Visualizar el predict del modelo LIVE------------
+        # -----------------------------------------------------
+        
+        while True:
+        
+            # Saving captured image and transforming from BGR TO RGB
+        
+            _, img = cap.read()
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            imgRGB = cv2.resize(imgRGB, (320, 180))
+            imgRGB = imgRGB.astype(int)/255
+        
+            y_predicted = self.ho_model.predict(np.array([imgRGB]), verbose=None)
+            prediction = np.argmax(y_predicted)
+            print(prediction)
+        
+            # Show the complete image
+            cv2.imshow('Image', img)
+        
+            key = cv2.waitKey(30)
+            if key == 27: # 13 = enter, 27 = esc
+                break
