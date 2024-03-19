dad122e60fceefae86e7b19ef1ee0b31bac80241
fue19389 <fue19389@uvg.edu.gt>
2024-03-19 01:02:04 -0600

    Funciona Webots, al menos con mi cara en la noche en mi lugar de trabajo

--- ho_sim/controllers/face_or/face_or.py
+++ ho_sim/controllers/face_or/face_or.py
@@ -1,16 +1,50 @@
 from controller import Robot, Motor
+import tensorflow as tf
+import numpy as np
+import cv2
 
-TIME_STEP = 64
+TIME_STEP = 32
+MAX_SPEED = 6.28
+CAP = cv2.VideoCapture(0)
+ho_model = r'C:\Users\gerar\PycharmProjects\EXPOR_TESIS\head_or8.keras'
+ho_model = tf.keras.models.load_model(ho_model)
 
 # create the Robot instance.
 robot = Robot()
 
-# get the motor devices
+# get a handler to the motors and set target position to infinity (speed control)
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
-# set the target position of the motors
-leftMotor.setPosition(10.0)
-rightMotor.setPosition(10.0)
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
 
-while robot.step(TIME_STEP) != -1:
-   pass
+
+while True:
+   _, img = CAP.read()
+   imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+   imgRGB = cv2.resize(imgRGB, (320, 180))
+   imgRGB = imgRGB.astype(int) / 255
+
+   y_predicted = ho_model.predict(np.array([imgRGB]), verbose=None)
+   prediction = int(np.argmax(y_predicted))
+
+   if prediction == 0:
+      leftMotor.setVelocity(0.25 * MAX_SPEED)
+      rightMotor.setVelocity(0.45 * MAX_SPEED)
+      robot.stepBegin(TIME_STEP)
+   if prediction == 1:
+      leftMotor.setVelocity(0.0 * MAX_SPEED)
+      rightMotor.setVelocity(0.0 * MAX_SPEED)
+      robot.stepBegin(TIME_STEP)
+   if prediction == 2:
+      leftMotor.setVelocity(0.45 * MAX_SPEED)
+      rightMotor.setVelocity(0.25 * MAX_SPEED)
+      robot.stepBegin(TIME_STEP)
+   if prediction == 3:
+      leftMotor.setVelocity(0.45 * MAX_SPEED)
+      rightMotor.setVelocity(0.45 * MAX_SPEED)
+      robot.stepBegin(TIME_STEP)
+
+   robot.stepEnd()
+
+
