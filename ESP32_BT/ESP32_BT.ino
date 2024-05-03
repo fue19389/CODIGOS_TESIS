e46f4cbf141149f051d059894c6b340be801bc4b
YRAR <fue19389@uvg.edu.gt>
2024-05-02 23:29:24 -0600

    Codigos para BLUETOOTH, pero creo que el BT de mi compu falla, tampoco funcionaba con mis audifonos xd

--- ESP32_BT/ESP32_BT.ino
+++ ESP32_BT/ESP32_BT.ino
@@ -1,16 +1,72 @@
-#include "BluetoothSerial.h"
+#include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
+#define FORWARD 12
+#define RIGHT 27
+#define LEFT 25
+#define BACKWARD 32
+
+int time_delay = 1000;
+int flag = 0;
+int count = 0;
 
 void setup() {
-  Serial.begin(115200);
   SerialBT.begin("ESP32_BT"); // Bluetooth device name
+  pinMode(FORWARD, OUTPUT);
+  pinMode(RIGHT, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  pinMode(BACKWARD, OUTPUT);
 }
+
 
 void loop() {
   if (SerialBT.available()) {
-    int receivedInt = SerialBT.read(); // Read the incoming byte as an integer
-    Serial.print("Received: ");
-    Serial.println(receivedInt);
+    int predictInt = SerialBT.read();
+
+    if (predictInt == 0) {
+        digitalWrite(LEFT, HIGH); 
+    }
+    else if (predictInt == 1){
+        digitalWrite(LEFT, LOW); 
+        digitalWrite(RIGHT, LOW); 
+        flag = 0;
+    }
+    else if (predictInt == 2){
+        digitalWrite(RIGHT, HIGH); 
+    }
+    else if (predictInt == 3){
+        if (flag == 0){
+          count = count + 1;
+          flag = 1;
+        }
+    }
+    else if (predictInt == 4){
+        if (flag == 0){
+          count = count - 1;
+          flag = 1;
+        }
+    }
+    
+    // Adjust count to be within range [-4, 4]
+    if (count > 4) {
+      count = 4;
+    } else if (count < -4) {
+      count = -4;
+    }
+    
+    if (count > 0) {
+      digitalWrite(FORWARD, HIGH);
+      delay(time_delay / count); 
+      digitalWrite(FORWARD, LOW);  
+      delay(time_delay / count);    
+    } else if (count < 0) {
+      digitalWrite(BACKWARD, HIGH);
+      delay(time_delay / (-count)); 
+      digitalWrite(BACKWARD, LOW);  
+      delay(time_delay / (-count));         
+    } else {
+      digitalWrite(FORWARD, LOW); 
+      digitalWrite(BACKWARD, LOW);
+    }
   }
 }
--- sendBT.py
+++ sendBT.py
@@ -1,16 +1,36 @@
-import
+import socket
+import tensorflow as tf
+import numpy as np
+import cv2
+import faceLANDMARKS as fL
+
 # MAC address of the ESP32 Bluetooth device
-esp32_mac_address = "CC:50:E3:96:CC:50"  # Replace this with your ESP32's MAC address
+esp32_mac_address = "CC:50:E3:96:CC:50"  # Replace with your ESP32's MAC address
+# Create a Bluetooth socket object
 
-# Establish a Bluetooth connection
-sock = pyblue.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((esp32_mac_address, 1))
+CAP = cv2.VideoCapture(0)
+ho_model = r'C:\Users\gerar\PycharmProjects\EXPOR_TESIS\head_or12.keras'
+ho_model = tf.keras.models.load_model(ho_model)
+detector = fL.FaceMeshDetector()
+flag = 0
 
-# Data to be sent
-data = 42
 
-# Convert integer to bytes before sending
-sock.send(bytes([data]))
+while True:
 
-# Close the connection
-sock.close()
+    sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+    _, imgF = CAP.read()
+    img, nodes = detector.findFaceMesh(imgF)
+    nodes = np.array([nodes])
+    if nodes.any() != 0:
+       y_predicted = ho_model.predict(nodes, verbose=None)
+       prediction = int(np.argmax(y_predicted))
+    else:
+       prediction = 1
+
+    # Connect to the ESP32 using channel 4
+    sock.connect((esp32_mac_address, 4))
+    # Convert integer to bytes before sending
+    data_bytes = prediction.to_bytes(4, byteorder='little')  # Assuming integer is 32 bits (4 bytes)
+    # Send the data
+    sock.sendall(data_bytes)
